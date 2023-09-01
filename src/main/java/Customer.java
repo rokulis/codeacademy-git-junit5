f7df670fda3a64a4828ceaf65aa6ffe2c55eb203
DariusZviedris <darius.zviedris@gmail.com>
2023-09-01 21:54:36 +0300

    Pakeitimai pagal komentarus 3

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,5 +1,3 @@
-
-
 public class Customer {
     String name;
     String email;
---
+++ src/test/java/datatest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class datatest {
+//    Customer custas = new Customer();
+
+    @Test
+    public void testD() {
+        Cart cart =new Cart();
+        Product p1 = new Product("Apple", 1.2);
+//        Product p2 = new Product("Orange", 0.8);
+//        cart.addProduct(p1);
+//       cart.addProduct(p2);
+
+
+   //     assertEquals(5, calculator.sudeti(2, 3));
+//        assertEquals(6, calculator.sudeti(2, 3));
+    }
+
+
+}
