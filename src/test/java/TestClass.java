e69dcae12955c870b90d1146b79ff35a516e1892
Rokas Ulickas <rokas@recodin.lt>
2023-09-04 11:01:08 +0300

    uncommited changes

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestClass {
+
+    @Test
+    public void test() {
+        System.out.println();
+        assertEquals(5, 5);
+    }
+}
